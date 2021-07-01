f1df90e7755d3ada40b02113578239e85e41b0e4
Omaru Jawara <omaru.jawara94@gmail.com>
2021-06-30 20:03:40 -0400

    Initial Commit

---
+++ IOS_calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS_calculator
+//
+//  Created by Omaru Jawara on 6/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

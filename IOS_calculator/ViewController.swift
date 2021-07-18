346bdbe74432f0b2d2d21f093e8a8580f08b16b2
Omaru Jawara <omarito.jawara@gmail.com>
2021-07-18 08:13:14 -0400

    Make bill amount textfield the first responder

--- IOS_calculator/ViewController.swift
+++ IOS_calculator/ViewController.swift
@@ -7,12 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var billAmountTextField: UITextField!
     @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
-    @IBOutlet weak var tipControl: UISegmentedControl!
     
     @IBOutlet weak var sliderValueLabel: UILabel!
     @IBOutlet weak var sliderValue: UISlider!
@@ -20,21 +19,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.title = "TIPP"
+        sliderValue.value = 15
+        sliderValueLabel.text! = String(Int(sliderValue.value)) + "%"
         billAmountTextField.placeholder = "Bill Amount..."
         billAmountTextField.backgroundColor = .secondarySystemBackground
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        billAmountTextField.becomeFirstResponder()
+    }
+    
+    
+    @IBAction func textEdit(_ sender: UITextField) {
+        calculate()
+    }
     @IBAction func sliderValue(_ sender: UISlider) {
-        
+        calculate()
+    }
+    
+    func calculate(){
         let bill = Float(billAmountTextField.text!)  ?? 0
-        sliderValueLabel.text! = String(Int(sender.value)) + "%"
-        
+        sliderValueLabel.text! = String(Int(sliderValue.value)) + "%"
         let tip = (Float(billAmountTextField.text!)  ?? 0) * (sliderValue.value/100)
         tipAmountLabel.text = String(format: "$%.2f",tip)
         
         let total = bill + tip
         totalLabel.text = String(format: "$%.2f", total)
-    
     }
     
 }
